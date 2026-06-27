6e03d75cb33a7c0d0a28013c05ad192b8e14323d
Quang Huy <quanghuy1472005@gmail.com>
2026-06-28 02:28:05 +0700

    repository Auth + Role

--- src/main/java/com/library/management/repositories/AuthorRepository.java
+++ src/main/java/com/library/management/repositories/AuthorRepository.java
@@ -1,9 +1,12 @@
 package com.library.management.repositories;
 
+import com.library.management.dto.LoginRequestDTO;
 import com.library.management.entities.Author;
+import com.library.management.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
 }
