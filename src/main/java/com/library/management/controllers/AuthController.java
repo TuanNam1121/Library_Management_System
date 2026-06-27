dd6f98e96744b91e7a90cb021ee0aa1cc4b8bf37
Quang Huy <quanghuy1472005@gmail.com>
2026-06-28 02:27:13 +0700

    AuthController

---
+++ src/main/java/com/library/management/controllers/AuthController.java
@@ -0,0 +1,77 @@
+package com.library.management.controllers;
+
+import com.library.management.dto.LoginRequestDTO;
+import com.library.management.dto.RegisterRequestDTO;
+import com.library.management.services.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auths")
+@RequiredArgsConstructor
+
+public class AuthController {
+private final UserService userService;
+
+    @GetMapping("/register")
+    public String registerProcess(Model model) {
+        model.addAttribute("user", new RegisterRequestDTO());
+        return "auths/register";
+    }
+
+    @PostMapping("/register")
+    public String register(
+            @Valid @ModelAttribute("user") RegisterRequestDTO dto,
+            BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "auths/register";
+        }
+
+        try {
+            userService.register(dto);
+        } catch (RuntimeException ex) {
+            result.reject("registerError", ex.getMessage());
+            return "auths/register";
+        }
+
+        return "redirect:/auths/login";
+    }
+
+
+    @GetMapping("/login")
+    public String loginProcess(Model model){
+        model.addAttribute("user",new  LoginRequestDTO());
+        return "auths/login";
+    }
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute("user") LoginRequestDTO loginRequestDTO,
+                        BindingResult result,
+                        Model model){
+        if(result.hasErrors()){
+            return "auth/login";
+        }
+        try {
+            boolean success = userService.login(loginRequestDTO);
+
+            if (!success) {
+                result.reject("loginError", "Invalid username or password");
+                return "auths/login";
+            }
+
+        } catch (RuntimeException ex) {
+            result.reject("loginError", ex.getMessage());
+            return "auths/login";
+        }
+        return "redirect:/books";
+    }
+    }
+
+
